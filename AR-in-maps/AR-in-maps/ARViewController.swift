01fce8309dabfa4beda998c412340ec53fa831c3
Kristina Gelžinytė <kristina.gelzinyte@gmail.com>
2018-01-08 17:32:08 -0800

    Added session delegate for ARKit session.

--- AR-in-maps/AR-in-maps/ARViewController.swift
+++ AR-in-maps/AR-in-maps/ARViewController.swift
@@ -11,29 +11,61 @@ import SceneKit
 import ARKit
 import MapKit
 
-class ARViewController: UIViewController, ARSCNViewDelegate {
+class ARViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     @IBOutlet weak var mapButton: UIButton!
     
     public var dropPinPoint: CLLocationCoordinate2D?
     
+    @IBOutlet weak var sessionInfoView: UIView!
+    @IBOutlet weak var sessionInfoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        guard ARWorldTrackingConfiguration.isSupported else {
+            fatalError("""
+                ARKit is not available on this device. For apps that require ARKit
+                for core functionality, use the `arkit` key in the key in the
+                `UIRequiredDeviceCapabilities` section of the Info.plist to prevent
+                the app from installing. (If the app can't be installed, this error
+                can't be triggered in a production scenario.)
+                In apps where AR is an additive feature, use `isSupported` to
+                determine whether to show UI for launching AR experiences.
+            """) // For details, see https://developer.apple.com/documentation/arkit
+        }
         
         setup(mapButton)
         
         // Set the view's delegate
-        sceneView.delegate = self
+//        sceneView.delegate = self
         
         // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
+//        sceneView.showsStatistics = true
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
         // Set the scene to the view
-        sceneView.scene = scene
+//        sceneView.scene = scene
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        // Enable plane detection
+        configuration.planeDetection = .horizontal
+        configuration.isLightEstimationEnabled = true
+        // Run the view's session
+        sceneView.session.run(configuration)
+        
+        // Set a delegate to track the number of plane anchors for providing UI feedback.
+        sceneView.session.delegate = self
+        
+        UIApplication.shared.isIdleTimerDisabled = true
+        
+        // Show debug UI to view performance metrics (e.g. frames per second).
+        sceneView.debugOptions = ARSCNDebugOptions.showFeaturePoints
+        
+
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -57,31 +89,79 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-
-    // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+    // MARK: - ARSessionDelegate
+    
+    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        guard let frame = session.currentFrame else { return }
+        updateSessionInfoLabel(for: frame, trackingState: frame.camera.trackingState)
     }
-*/
+    
+    func session(_ session: ARSession, didRemove anchors: [ARAnchor]) {
+        guard let frame = session.currentFrame else { return }
+        updateSessionInfoLabel(for: frame, trackingState: frame.camera.trackingState)
+    }
+    
+    func session(_ session: ARSession, cameraDidChangeTrackingState camera: ARCamera) {
+        updateSessionInfoLabel(for: session.currentFrame!, trackingState: camera.trackingState)
+    }
+    
+    
+    // MARK: - ARSessionObserver
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
-        
+        sessionInfoLabel.text = "Session failed: \(error.localizedDescription)"
+        resetTracking()
     }
     
     func sessionWasInterrupted(_ session: ARSession) {
         // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
+        sessionInfoLabel.text = "Session was interrupted"
     }
     
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
+        sessionInfoLabel.text = "Session interruption ended"
+        resetTracking()
+    }
+    
+    // MARK: - Private methods
+    
+    private func updateSessionInfoLabel(for frame: ARFrame, trackingState: ARCamera.TrackingState) {
+        // Update the UI to provide feedback on the state of the AR experience.
+        let message: String
         
+        switch trackingState {
+        case .normal where frame.anchors.isEmpty:
+            // No planes detected; provide instructions for this app's AR interactions.
+            message = "Move the device around to detect horizontal surfaces."
+            
+        case .normal:
+            // No feedback needed when tracking is normal and planes are visible.
+            message = ""
+            
+        case .notAvailable:
+            message = "Tracking unavailable."
+            
+        case .limited(.excessiveMotion):
+            message = "Tracking limited - Move the device more slowly."
+            
+        case .limited(.insufficientFeatures):
+            message = "Tracking limited - Point the device at an area with visible surface detail, or improve lighting conditions."
+            
+        case .limited(.initializing):
+            message = "Initializing AR session."
+        }
+        
+        sessionInfoLabel.text = message
+        sessionInfoView.isHidden = message.isEmpty
+    }
+    
+    private func resetTracking() {
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
     }
     
     private func setup(_ button: UIButton){
